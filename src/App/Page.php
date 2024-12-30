abe7e95dd367ee0a48480258937e85219aa8bf6d
Art4 <art4@wlabs.de>
2024-12-30 14:10:34 +0000

    Replace usage of DataFilterEvent with HtmlFilterEvent

--- src/App/Page.php
+++ src/App/Page.php
@@ -21,7 +21,7 @@ use Friendica\Core\Renderer;
 use Friendica\Core\Session\Model\UserSession;
 use Friendica\Core\System;
 use Friendica\Core\Theme;
-use Friendica\Event\DataFilterEvent;
+use Friendica\Event\HtmlFilterEvent;
 use Friendica\Network\HTTPException;
 use Friendica\Util\Images;
 use Friendica\Util\Network;
@@ -233,9 +233,10 @@ class Page implements ArrayAccess
 			$touch_icon = 'images/friendica-192.png';
 		}
 
-		$this->page['htmlhead'] = $this->eventDispatcher->dispatch(
-			new DataFilterEvent('head', $this->page['htmlhead'])
-		)->getData();
+		$this->page['htmlhead'] = $this->eventDispatcher->dispatch(new HtmlFilterEvent(
+			HtmlFilterEvent::HEAD,
+			$this->page['htmlhead']
+		))->getHtml();
 
 		$tpl = Renderer::getMarkupTemplate('head.tpl');
 		/* put the head template at the beginning of page['htmlhead']
@@ -357,9 +358,10 @@ class Page implements ArrayAccess
 			]);
 		}
 
-		$this->page['footer'] = $this->eventDispatcher->dispatch(
-			new DataFilterEvent('footer', $this->page['footer'])
-		)->getData();
+		$this->page['footer'] = $this->eventDispatcher->dispatch(new HtmlFilterEvent(
+			HtmlFilterEvent::FOOTER,
+			$this->page['footer']
+		))->getHtml();
 
 		$tpl                  = Renderer::getMarkupTemplate('footer.tpl');
 		$this->page['footer'] = Renderer::replaceMacros($tpl, [
@@ -383,9 +385,10 @@ class Page implements ArrayAccess
 	{
 		// initialise content region
 		if ($mode->isNormal()) {
-			$this->page['content'] = $this->eventDispatcher->dispatch(
-				new DataFilterEvent('page_content_top', $this->page['content'])
-			)->getData();
+			$this->page['content'] = $this->eventDispatcher->dispatch(new HtmlFilterEvent(
+				HtmlFilterEvent::PAGE_CONTENT_TOP,
+				$this->page['content']
+			))->getHtml();
 		}
 
 		$this->page['content'] .= (string)$response->getBody();
@@ -483,9 +486,10 @@ class Page implements ArrayAccess
 		$profiler->set(microtime(true) - $timestamp, 'aftermath');
 
 		if (!$mode->isAjax()) {
-			$this->page['content'] = $this->eventDispatcher->dispatch(
-				new DataFilterEvent('page_end', $this->page['content'])
-			)->getData();
+			$this->page['content'] = $this->eventDispatcher->dispatch(new HtmlFilterEvent(
+				HtmlFilterEvent::PAGE_END,
+				$this->page['content']
+			))->getHtml();
 		}
 
 		// Add the navigation (menu) template
